8c398538170e61311d3f2f2a1774074a6292662d
glendc <glen.decauwsemaecker@otainsight.com>
2021-09-07 22:48:03 +0200

    update LICENSE & COPYRIGHT text

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! Bucket Rust Library, serving at the core of Bucket,
+//! a FOSS positive time tracker, allowing you to track time,
+//! one drop at a time, filling your bucket week by week,
+//! working towards growth and beyond.
+
+/*
+Bucket, a FOSS positive time tracker, one drop at a time.
+Copyright (C) 2021  Glen Henri J. De Cauwsemaeker and Elizabeth C. Gonzales Belsuzarri.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 #[cfg(test)]
 mod tests {
     #[test]
