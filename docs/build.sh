1f20e092ee1b82b46a5e38e479bea279b9803bd4
glendc <glen.decauwsemaecker@otainsight.com>
2021-09-08 09:17:41 +0200

    add echo statements to build script for feedback

--- docs/build.sh
+++ docs/build.sh
@@ -6,27 +6,47 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ORIGINAL_DIR="${PWD}"
 
 # build
+echo "building bucket website..."
+echo
 cd "$SCRIPT_DIR/../bucket-website"
 elm-spa build
+echo "-----------------------------------------------------------"
+echo
 
 # copy files
+echo "copying bucket website content from ${SCRIPT_DIR}/../bucket-website/public ..."
+echo
 cd "$SCRIPT_DIR"
 cp -a "${SCRIPT_DIR}/../bucket-website/public/." .
+echo "-----------------------------------------------------------"
+echo
 
 # go back to original dir
 cd "$ORIGINAL_DIR"
 
+echo "git publishing main (branch) content ..."
+echo
 # commit (msg via stdin)
 git add -A
 # ... and publish
 git push origin main
+echo "-----------------------------------------------------------"
+echo
 
+echo "git publishing pages (branch) content ..."
+echo
 # checkout pages, merge main, commit & push
 git checkout pages
 git merge main
 git add -A
 git commit
 git push origin pages
+echo "-----------------------------------------------------------"
+echo
 
+echo "Done ..."
+echo
 # back to main page
 git checkout main
+
+echo "Bye :)"
