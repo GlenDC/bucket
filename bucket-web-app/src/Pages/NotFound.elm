0b8dc6b97ea58871c901cc8873ce1d81b6f186ac
glendc <glen.decauwsemaecker@otainsight.com>
2021-09-11 22:30:16 +0200

    improve 404 web app page

--- bucket-web-app/src/Pages/NotFound.elm
+++ bucket-web-app/src/Pages/NotFound.elm
@@ -1,8 +1,16 @@
 module Pages.NotFound exposing (view)
 
+import Html
+import UI
 import View exposing (View)
-
 
 view : View msg
 view =
-    View.placeholder "Drop not found."
+    { title = "Bucket App — 404"
+    , body = UI.layout [ Html.text
+        """
+        This drop could not be found,
+        look inwards and continue your journey :)
+        """
+        ]
+    }
